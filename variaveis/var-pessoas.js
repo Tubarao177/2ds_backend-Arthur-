904bd80b7595ae91f578c40dc71629a33e696040
Tubarao177 <arthur.pereira.muller@escola.pr.gov.br>
2026-02-25 13:28:27 +0000

    variaveis para pessoas

---
+++ variaveis/var-pessoas.js
@@ -0,0 +1,20 @@
+// , var, let, const
+// tipos de dados
+
+let nome = "Bruce"         //string
+let sobrenome = "Wayne"   //string
+const cpf = "88888888-98" //string
+const rg = "131231 ssp"  //string
+let idade = 22           //number
+let solteiro = true      //boolear
+let nota = 5 + 3         //number
+
+console.log (nome)
+console.log (sobrenome)
+console.log (cpf)
+console.log (rg)
+console.log (idade)
+console.log (solteiro)
+console.log (nota)
+
+
