eab3046b3fffd042f6e7de5f2bce080ec2169637
Tubarao177 <arthur.pereira.muller@escola.pr.gov.br>
2026-03-09 13:25:57 +0000

    Introdução aos Arrays em JS

---
+++ condicionais/arrays/arays-time.js
@@ -0,0 +1,10 @@
+// arays: são agrupamentos de valores em forma de lista
+// São indexadas a partir de ZERO até o infinito
+// Podem ter homegêneos/ heterogêneos 
+// Possuem Metofod (funções) próprios
+
+const time = ['Petter', 'Mariah']
+// indeices      0         1
+
+console.log(time[1])
+console.log(time[32]) //undefined
---
+++ condicionais/if-else.js
@@ -0,0 +1,18 @@
+// if, else, else if, ternário
+// <, >, <=, >=, !=
+// ||, &&
+
+let idade = 19
+
+if (idade > 110) {
+    console.log("")
+}
+
+if (idade >=18) {
+    console.log("Entrada Liberada")
+    console.log("Festa Sugerida: Balada!!!")
+
+}else{
+    console.log("Proibido, vaza peitozin de roda pé")
+    console.log("Festa Sugerida, Monica Toy")
+}
