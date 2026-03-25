d1a894f8b81813dbc95d39e7df6880a389e580e6
Tubarao177 <arthur.pereira.muller@escola.pr.gov.br>
2026-03-25 11:36:52 +0000

    Finalização de objetos JS

---
+++ objetos/obj-01.js
@@ -0,0 +1,16 @@
+// Objetos são estruturas chave-valor
+// Também chamadas de "Key-value"
+// Objetos podem conter: arrays, string,
+// number, objects, functions...
+// Possuem metodos próprios
+
+const jogador = {
+    nome: "Tubarão",
+    gols: 1945,
+    altura: 1.95,
+    time: ['Palmeiras', 'Coxa', 'Flamengo', 'Corinthians']
+}
+console.log(jogador.nome)
+console.log(jogador.gols)
+console.log(jogador.time)
+console.log(jogador.altura)
---
+++ objetos/obj-02.js
@@ -0,0 +1,20 @@
+// A partir do objeto de exemplo crie;
+// Um objeto com o tema da sua escolha
+// Deve possuir no mínimo 6 propriedades
+// Imprima de um jeito amigável:
+// Ex.: console.log("Cor do veículo:" + carro.cor)
+
+const jogos = {
+    nome: "Red Dead Redeption 2",
+    ano: "2018",
+    tipo: "Ação",
+    diretor: "Joe Rubino",
+    premios: " 175 prêmios de Jogo do Ano (GOTY)",
+    empresa: "  Rockstar Games",
+}
+console.log(jogos.ano)
+console.log(jogos.nome)
+console.log(jogos.diretor)
+console.log(jogos.tipo)
+console.log(jogos.premios)
+console.log(jogos.empresa)
