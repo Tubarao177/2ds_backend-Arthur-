3db6747f07788a4248c0b90592e5d4b5ef740472
Tubarao177 <arthur.pereira.muller@escola.pr.gov.br>
2026-06-17 11:38:44 +0000

    Colocando cores nas mensagens

--- 2°Trimestre/Módulos/fs/mod-fs-path.js
+++ 2°Trimestre/Módulos/fs/mod-fs-path.js
@@ -1,13 +1,13 @@
 const fs = require('fs')
 const path = require('path')
-const chalkv = require('chalk').default
+const chalk = require('chalk').default // padrão
 try {
     const arquivo = fs.readFileSync(path.join(__dirname, '/festa.md'), 'utf-8')
-   console.log(chalk.green('Arquivo carregado com sucesso!!!'))
+   console.log(chalk.blue.bgBlackBright.bold(' 👹 Arquivo carregado com sucesso!!!'))
     console.log(arquivo)
 }
 catch (erro) {
-    console.log(chalk.red("Deu ruim!"))
+    console.log(chalkblue.underline.bold(" Deu ruim!"))
     console.log("Deu ruim: \n" + erro.message)
 }
 
